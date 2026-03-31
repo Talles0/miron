f7c3649bc4bcd567136dc33c24f8192c7073ea60
Talles Fernando <93330018+Talles0@users.noreply.github.com>
2026-03-31 17:25:07 -0300

    Versão 1.1
    Melhorado visual e começado IMC

--- Miron/script.js
+++ Miron/script.js
@@ -1,5 +1,9 @@
 document.getElementById("header-top").innerHTML = "Miron Calculador"
 
+function erro(){
+    window.location.reload()
+}
+
 function calcularIdade(){
     var dia_av, mes_av, ano_av, dia_nas, mes_nas, ano_nas, ano_bissexto, divisor
     var primeira_fracao, segunda_fracao, idade, idadeStr
@@ -43,4 +47,33 @@ function calcularIdade(){
         idadeStr = idadeStr.slice(0, 6)
     }
     document.getElementById("idade-real").innerHTML = idadeStr
+
+    mes_av = mes_av + 1
+    dia_av = dia_av + 1
+    mes_nas = mes_nas + 1
+    dia_nas = dia_nas + 1
+
+    if (idade < 0 ||
+    dia_av < 1 || dia_av > 31 ||
+    dia_nas < 1 || dia_nas > 31 ||
+    mes_av < 1 || mes_av > 12 ||
+    mes_nas < 1 || mes_nas > 12 ||
+    ano_av < 0 || ano_nas < 0) {
+    alert("Insira dados válidos!")
+    erro()
 }
+}
+
+// function calcularImc() {
+//     var peso = Number(document.getElementById("peso").value);
+//     var altura = Number(document.getElementById("altura").value);
+
+//     // Verifica se os valores são números válidos e positivos
+//     if (isNaN(peso) || isNaN(altura) || peso <= 0 || altura <= 0) {
+//         document.getElementById("imc-resultado").innerHTML = "Por favor, insira peso e altura válidos (positivos).";
+//         return;
+//     }
+
+//     var imc = peso / Math.pow(altura, 2);
+//     document.getElementById("imc-resultado").innerHTML = imc.toFixed(2);
+// }
